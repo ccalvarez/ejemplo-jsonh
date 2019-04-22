30d8fe75162d5c8cc3437acac61353d6879ae699
Carolina Cordero <ccalvarez@gmail.com>
2019-04-22 11:37:08 -0600

    feat: compacta municipal-2016-sin-juntas.json

--- js/comprimir.js
+++ js/comprimir.js
@@ -1,21 +1,24 @@
 const fs = require('fs');
 const jsonh = require('./jsonh');
-let original = fs.readFileSync('un-distrito.json', 'utf8');
+let original = fs.readFileSync('municipal-2016-sin-juntas.json', 'utf8');
 
 let parsed = JSON.parse(original);
 
-// la librería jsonh opera sólo sobre arrays, entonces metí el distrito dentro de un array
-Array.from(parsed).forEach(element => {
-  // element es en realidad el único elemento de nuestro array (el distrito)
-
-  Array.from(element.j).forEach(j => {
-    j.v = jsonh.pack(j.v); // compacta el arreglo de votos de cada junta
+Array.from(parsed.e).forEach(e => {
+  Array.from(e.l).forEach(l => {
+    l.v = jsonh.pack(l.v);
   });
-  element.j = jsonh.pack(element.j); // compacta la junta
-  element.v = jsonh.pack(element.v); // compacta el arreglo de votos del distrito
+  e.l = jsonh.pack(e.l);
 });
+parsed.e = jsonh.pack(parsed.e);
 
-parsed = jsonh.pack(parsed); // compacta los distritos, en este caso sólo tenemos un distrito en el arreglo principal
+// console.log(JSON.stringify(parsed));
+console.log(
+  'creado/actualizado el archivo municipal-2020-sin-juntas-compacto.json'
+);
 
-console.log(JSON.stringify(parsed));
+fs.writeFileSync(
+  'municipal-2016-sin-juntas-compacto.json',
+  JSON.stringify(parsed)
+);
 // console.log(parsed);
