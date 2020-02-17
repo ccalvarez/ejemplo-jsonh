c1121ac51fd06f73b825811bc5865a5a802ce02c
Carolina Cordero <ccalvarez@gmail.com>
2020-02-17 16:49:40 -0600

    agrega archivos del corte 61

--- js/descomprimir.js
+++ js/descomprimir.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const jsonh = require('./jsonh');
 
 // El archivo 'Corte con compresión JSONH.json' se crea ejecutando en la terminal de comandos:  node comprimir
-let compacto = fs.readFileSync('corte20escrutinio.json', 'utf8');
+let compacto = fs.readFileSync('corte61escrutiniocomp.json', 'utf8');
 let datos = JSON.parse(compacto);
 
 datos.e = jsonh.unpack(datos.e);
@@ -12,7 +12,7 @@ Array.from(datos.e).forEach(e => {
     l.v = jsonh.unpack(l.v);
   });
   fs.writeFileSync(
-    'corte20escrutiniodescomp' + e.id.toString() + '.json',
+    'corte61escrutiniodescomp' + e.id.toString() + '.json',
     JSON.stringify(e.l)
   );
 });
@@ -22,4 +22,4 @@ console.log(
   'creado/actualizado el archivo "Corte descomprimido con JSONH.json"'
 );
 
-fs.writeFileSync('corte20escrutiniodescomp.json', JSON.stringify(datos));
+fs.writeFileSync('corte61escrutiniodescomp.json', JSON.stringify(datos));
